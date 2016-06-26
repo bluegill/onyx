77c0c3398b581c7bcba6a49be06139c88efd316c
Gage <bluegill@users.noreply.github.com>
2016-06-25 23:39:25 -0400

    reverse kick if rank >=

--- src/handlers/handleKick.js
+++ src/handlers/handleKick.js
@@ -2,6 +2,10 @@ module.exports = (data, client, world) => {
   const id = data[3];
   if(world.isOnline(id) && client.isModerator){
     let player = world.getClientById(id);
+    if(player.rank >= client.rank){
+      client.sendError(5);
+      return world.removeClient(client);
+    }
     player.sendError(5);
     world.removeClient(player);
   }
