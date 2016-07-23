3dc31885b050475d9e22af99da6a51b1a8f712f4
Gage <bluegill@users.noreply.github.com>
2016-07-22 23:37:59 -0400

    ???

--- src/Plugins/PluginBase.js
+++
@@ -1,9 +0,0 @@
-import logger from '../Logger';
-
-export default class {
-  constructor(manager){
-    this.parent = manager;
-    this.world  = manager.world;
-    this.server = manager.server;
-  }
-}
