28b34a46ce847c839c6b064ae08c9a85b2b456c5
bluegill <bluegill@users.noreply.github.com>
2017-07-06 23:39:49 -0400

    OCD

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -4,10 +4,10 @@
 // npm start [server id]
 // or node Bootstrap.js [server id]
 
-import Logger from './Utilities/Logger'
-
-import Onyx from './Server'
 import * as Config from './Config'
+
+import Logger from './Utilities/Logger'
+import Onyx from './Server'
 
 if (Config.SERVER_ID) {
   const Instance = new Onyx(Config)
